210a167d4d89ed30f0de368ff3a36a0454dded3a
Yuri Spaziani <adummio@gmail.com>
2026-06-04 09:57:11 +0200

    Closes #7

--- BlazorWebApp/Services/IPokedexClientService.cs
+++ BlazorWebApp/Services/IPokedexClientService.cs
@@ -2,5 +2,6 @@ using Pokemon.Core;
 
 public interface IPokedexClientService
 {
-    Task<bool> SalvaPokemonAsync(string trainerId, PokemonModel pokemon);
+    Task<bool> SalvaPokemonAsync(string? trainerId, PokemonModel pokemon);
+    Task<List<PokemonModel>> GetPokedexAsync(string trainerId);
 }
