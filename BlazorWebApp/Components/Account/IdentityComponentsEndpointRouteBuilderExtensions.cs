76957a128875444ee96ecfe6fa29f81ab7e69d72
Yuri Spaziani <adummio@gmail.com>
2026-06-04 14:28:51 +0200

    closes #15

--- BlazorWebApp/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
+++ BlazorWebApp/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
@@ -48,7 +48,7 @@ internal static class IdentityComponentsEndpointRouteBuilderExtensions
         {
             await signInManager.SignOutAsync();
             return TypedResults.LocalRedirect($"~/{returnUrl}");
-        }).DisableAntiforgery();
+        }).DisableAntiforgery(); //Disable antiforgery per questo endpoint se no non fa fare il logout
 
         accountGroup.MapPost("/PasskeyCreationOptions", async (
             HttpContext context,
